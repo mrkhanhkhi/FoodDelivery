d2a4653ae2fce832df6e0f5c2afc4cf12a54b3f4
Nguyen Duy Khanh <>
2022-04-30 20:46:55 +0700

    build food pageview

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,4 +1,9 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/icon_and_text.dart';
+import 'package:food_delivery/widgets/small_text.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({Key? key}) : super(key: key);
@@ -8,8 +13,179 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
+  var _currentPageValue = 0.0;
+  double _scaleFactor = 0.85;
+  double _height = 220;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currentPageValue = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Container(
+          height: 320,
+          child: PageView.builder(
+              controller: pageController,
+              itemCount: 5,
+              itemBuilder: (context, position) {
+                return _buildPageItem(position);
+              }),
+        ),
+        DotsIndicator(
+          dotsCount: 5,
+          position: _currentPageValue,
+          decorator: DotsDecorator(
+            activeColor: AppColors.mainColor,
+            size: const Size.square(9.0),
+            activeSize: const Size(18.0, 9.0),
+            activeShape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0)),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildPageItem(int index) {
+    // Calculate the transformation value
+    Matrix4 matrix = Matrix4.identity();
+
+    if (index == _currentPageValue.floor()) {
+      var currentScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currentScale) / 2;
+
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currentPageValue.floor() + 1) {
+      var currentScale =
+          _scaleFactor + (_currentPageValue - index + 1) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currentScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currentPageValue.floor() - 1) {
+      var currentScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+
+      var currTrans = _height * (1 - currentScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else {
+      var currentScale = 0.85;
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1)
+        ..setTranslationRaw(0, _height * (1 - _scaleFactor) / 2, 1);
+    }
+
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: _height,
+            margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+                image: const DecorationImage(
+                    image: AssetImage('assets/image/food0.png'),
+                    fit: BoxFit.cover)),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 120,
+              margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+                boxShadow: const [
+                  BoxShadow(
+                      color: Colors.black12,
+                      offset: Offset(0, 5),
+                      blurRadius: 5),
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(0, 5),
+                  )
+                ],
+              ),
+              child: Container(
+                  padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const BigText(text: 'This is a fucking sandwich'),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Wrap(
+                            children: List.generate(
+                                5,
+                                (index) => const Icon(Icons.star,
+                                    color: AppColors.mainColor, size: 15)),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const SmallText(text: '4.5 km'),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const SmallText(text: '1000 comments')
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: const [
+                          IconAndText(
+                              icon: Icons.circle_sharp,
+                              text: "Normal",
+                              color: AppColors.textColor,
+                              iconColor: AppColors.iconColor1),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          IconAndText(
+                              icon: Icons.location_on,
+                              text: "1.7 km",
+                              color: AppColors.textColor,
+                              iconColor: AppColors.mainColor),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          IconAndText(
+                              icon: Icons.access_time_rounded,
+                              text: "Normal",
+                              color: AppColors.textColor,
+                              iconColor: AppColors.iconColor2)
+                        ],
+                      ),
+                    ],
+                  )),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:food_delivery/home/food_page_body.dart';
 import 'package:food_delivery/widgets/big_text.dart';
 import 'package:food_delivery/widgets/small_text.dart';
 
@@ -20,29 +21,33 @@ class _MainFoodPageState extends State<MainFoodPage> {
       children: [
         Container(
           child: Container(
-              margin: EdgeInsets.only(top: 45, bottom: 15),
-              padding: EdgeInsets.only(left: 20, right: 20),
+              margin: const EdgeInsets.only(top: 45, bottom: 15),
+              padding: const EdgeInsets.only(left: 20, right: 20),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Column(
-                    children: const [
-                      BigText(
+                    children: [
+                      const BigText(
                         text: 'Vietnam',
                         color: AppColors.mainColor,
                       ),
-                      SmallText(
-                        text: 'Hanoi',
-                        color: Colors.black54,
-                      ),
-                      Icon(Icons.arrow_drop_down_rounded)
+                      Row(
+                        children: const [
+                          SmallText(
+                            text: 'Hanoi',
+                            color: Colors.black54,
+                          ),
+                          Icon(Icons.arrow_drop_down_rounded),
+                        ],
+                      )
                     ],
                   ),
                   Center(
                     child: Container(
                       width: 45,
                       height: 45,
-                      child: Icon(
+                      child: const Icon(
                         Icons.search,
                         color: Colors.white,
                       ),
@@ -54,6 +59,7 @@ class _MainFoodPageState extends State<MainFoodPage> {
                 ],
               )),
         ),
+        const FoodPageBody()
       ],
     ));
   }
